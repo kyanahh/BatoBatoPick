19ea7dc075ec7976d4eeb3a492dc4a00dd4c0c6d
kyanahh <kiannadominiquealvarez@gmail.com>
2023-09-13 15:44:32 +0800

    first commit

---
+++ AlvarezBatoBatoPick/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿
+namespace AlvarezBatoBatoPick
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblResultOrig = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblComputer = new System.Windows.Forms.Label();
+            this.lblYou = new System.Windows.Forms.Label();
+            this.lblComputerOrig = new System.Windows.Forms.Label();
+            this.lblYouOrig = new System.Windows.Forms.Label();
+            this.btnGame = new System.Windows.Forms.Button();
+            this.lblGame = new System.Windows.Forms.Label();
+            this.rbGunting = new System.Windows.Forms.RadioButton();
+            this.rbPapel = new System.Windows.Forms.RadioButton();
+            this.rbBato = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("Good Times", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(128)))));
+            this.lblHeader.Location = new System.Drawing.Point(33, 46);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(430, 35);
+            this.lblHeader.TabIndex = 0;
+            this.lblHeader.Text = "BATO BATO PICK";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.lblResult);
+            this.panel1.Controls.Add(this.lblResultOrig);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.btnGame);
+            this.panel1.Controls.Add(this.lblGame);
+            this.panel1.Controls.Add(this.rbGunting);
+            this.panel1.Controls.Add(this.rbPapel);
+            this.panel1.Controls.Add(this.rbBato);
+            this.panel1.Controls.Add(this.lblHeader);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(494, 477);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel5.Location = new System.Drawing.Point(109, 310);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(271, 1);
+            this.panel5.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Location = new System.Drawing.Point(212, 275);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1, 94);
+            this.panel3.TabIndex = 9;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Good Times", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(128)))));
+            this.lblResult.Location = new System.Drawing.Point(3, 428);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 8;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblResultOrig
+            // 
+            this.lblResultOrig.AutoSize = true;
+            this.lblResultOrig.Font = new System.Drawing.Font("Good Times", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultOrig.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(128)))));
+            this.lblResultOrig.Location = new System.Drawing.Point(3, 402);
+            this.lblResultOrig.Name = "lblResultOrig";
+            this.lblResultOrig.Size = new System.Drawing.Size(99, 16);
+            this.lblResultOrig.TabIndex = 7;
+            this.lblResultOrig.Text = "Result:";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.lblComputer);
+            this.panel2.Controls.Add(this.lblYou);
+            this.panel2.Controls.Add(this.lblComputerOrig);
+            this.panel2.Controls.Add(this.lblYouOrig);
+            this.panel2.Location = new System.Drawing.Point(109, 275);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(271, 94);
+            this.panel2.TabIndex = 6;
+            // 
+            // lblComputer
+            // 
+            this.lblComputer.AutoSize = true;
+            this.lblComputer.Font = new System.Drawing.Font("Good Times", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComputer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(128)))));
+            this.lblComputer.Location = new System.Drawing.Point(168, 50);
+            this.lblComputer.Name = "lblComputer";
+            this.lblComputer.Size = new System.Drawing.Size(39, 29);
+            this.lblComputer.TabIndex = 3;
+            this.lblComputer.Text = "0";
+            // 
+            // lblYou
+            // 
+            this.lblYou.AutoSize = true;
+            this.lblYou.Font = new System.Drawing.Font("Good Times", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYou.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(128)))));
+            this.lblYou.Location = new System.Drawing.Point(34, 50);
+            this.lblYou.Name = "lblYou";
+            this.lblYou.Size = new System.Drawing.Size(39, 29);
+            this.lblYou.TabIndex = 2;
+            this.lblYou.Text = "0";
+            // 
+            // lblComputerOrig
+            // 
+            this.lblComputerOrig.AutoSize = true;
+            this.lblComputerOrig.Font = new System.Drawing.Font("Good Times", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComputerOrig.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(128)))));
+            this.lblComputerOrig.Location = new System.Drawing.Point(121, 11);
+            this.lblComputerOrig.Name = "lblComputerOrig";
+            this.lblComputerOrig.Size = new System.Drawing.Size(133, 16);
+            this.lblComputerOrig.TabIndex = 1;
+            this.lblComputerOrig.Text = "Computer";
+            // 
+            // lblYouOrig
+            // 
+            this.lblYouOrig.AutoSize = true;
+            this.lblYouOrig.Font = new System.Drawing.Font("Good Times", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYouOrig.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(128)))));
+            this.lblYouOrig.Location = new System.Drawing.Point(24, 11);
+            this.lblYouOrig.Name = "lblYouOrig";
+            this.lblYouOrig.Size = new System.Drawing.Size(56, 16);
+            this.lblYouOrig.TabIndex = 0;
+            this.lblYouOrig.Text = "You";
+            // 
+            // btnGame
+            // 
+            this.btnGame.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(128)))));
+            this.btnGame.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnGame.Font = new System.Drawing.Font("Good Times", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGame.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(100)))));
+            this.btnGame.Location = new System.Drawing.Point(23, 200);
+            this.btnGame.Name = "btnGame";
+            this.btnGame.Size = new System.Drawing.Size(449, 52);
+            this.btnGame.TabIndex = 5;
+            this.btnGame.Text = "GAME";
+            this.btnGame.UseVisualStyleBackColor = false;
+            this.btnGame.Click += new System.EventHandler(this.btnGame_Click);
+            // 
+            // lblGame
+            // 
+            this.lblGame.AutoSize = true;
+            this.lblGame.Font = new System.Drawing.Font("Good Times", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGame.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(128)))));
+            this.lblGame.Location = new System.Drawing.Point(186, 81);
+            this.lblGame.Name = "lblGame";
+            this.lblGame.Size = new System.Drawing.Size(107, 23);
+            this.lblGame.TabIndex = 4;
+            this.lblGame.Text = "Game";
+            // 
+            // rbGunting
+            // 
+            this.rbGunting.AutoSize = true;
+            this.rbGunting.Font = new System.Drawing.Font("Distant Galaxy", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbGunting.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(128)))));
+            this.rbGunting.Location = new System.Drawing.Point(332, 149);
+            this.rbGunting.Name = "rbGunting";
+            this.rbGunting.Size = new System.Drawing.Size(130, 27);
+            this.rbGunting.TabIndex = 3;
+            this.rbGunting.TabStop = true;
+            this.rbGunting.Text = "Gunting";
+            this.rbGunting.UseVisualStyleBackColor = true;
+            // 
+            // rbPapel
+            // 
+            this.rbPapel.AutoSize = true;
+            this.rbPapel.Font = new System.Drawing.Font("Distant Galaxy", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbPapel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(128)))));
+            this.rbPapel.Location = new System.Drawing.Point(183, 149);
+            this.rbPapel.Name = "rbPapel";
+            this.rbPapel.Size = new System.Drawing.Size(110, 27);
+            this.rbPapel.TabIndex = 2;
+            this.rbPapel.TabStop = true;
+            this.rbPapel.Text = "Papel";
+            this.rbPapel.UseVisualStyleBackColor = true;
+            // 
+            // rbBato
+            // 
+            this.rbBato.AutoSize = true;
+            this.rbBato.Font = new System.Drawing.Font("Distant Galaxy", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbBato.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(128)))));
+            this.rbBato.Location = new System.Drawing.Point(48, 149);
+            this.rbBato.Name = "rbBato";
+            this.rbBato.Size = new System.Drawing.Size(95, 27);
+            this.rbBato.TabIndex = 1;
+            this.rbBato.TabStop = true;
+            this.rbBato.Text = "Bato";
+            this.rbBato.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(100)))));
+            this.ClientSize = new System.Drawing.Size(518, 501);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Bato Bato Pick";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton rbGunting;
+        private System.Windows.Forms.RadioButton rbPapel;
+        private System.Windows.Forms.RadioButton rbBato;
+        private System.Windows.Forms.Button btnGame;
+        private System.Windows.Forms.Label lblGame;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblResultOrig;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblComputer;
+        private System.Windows.Forms.Label lblYou;
+        private System.Windows.Forms.Label lblComputerOrig;
+        private System.Windows.Forms.Label lblYouOrig;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel5;
+    }
+}
+
---
+++ AlvarezBatoBatoPick/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlvarezBatoBatoPick
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnGame_Click(object sender, EventArgs e)
+        {
+            Random ran = new Random();
+            int num = ran.Next(1, 4);
+            string compChoice = "";
+
+            if (num == 1)
+            {
+                compChoice = "Bato";
+            }else if(num == 2)
+            {
+                compChoice = "Papel";
+            }else if(num == 3)
+            {
+                compChoice = "Gunting";
+            }
+
+            lblResult.Text = compChoice;
+
+            int comp = Convert.ToInt32(lblComputer.Text);
+            int you = Convert.ToInt32(lblYou.Text);
+
+            while (!((comp == 3 && you < comp) || (you == 3 && comp < you))) { 
+                if (rbBato.Checked == true && compChoice == "Bato" || rbPapel.Checked == true && compChoice == "Papel" || rbGunting.Checked == true && compChoice == "Gunting")
+                {
+                    lblResult.Text = "It's a tie! Computer's choice is " + compChoice;
+                    rbBato.Checked = false;
+                    rbGunting.Checked = false;
+                    rbPapel.Checked = false;
+                    return;
+                }
+                else if (rbBato.Checked == true && compChoice == "Papel" || rbPapel.Checked == true && compChoice == "Gunting" || rbGunting.Checked == true && compChoice == "Bato")
+                {
+                    lblResult.Text = "Computer Wins! Computer's choice is " + compChoice;
+                    comp++;
+                    lblComputer.Text = comp.ToString();
+                    rbBato.Checked = false;
+                    rbGunting.Checked = false;
+                    rbPapel.Checked = false;
+                    return;
+                }
+                else if (rbBato.Checked == true && compChoice == "Gunting" || rbPapel.Checked == true && compChoice == "Bato" || rbGunting.Checked == true && compChoice == "Papel")
+                {
+                    lblResult.Text = "User Wins! Computer's choice is " + compChoice;
+                    you++;
+                    lblYou.Text = you.ToString();
+                    rbBato.Checked = false;
+                    rbGunting.Checked = false;
+                    rbPapel.Checked = false;
+                    return;
+                }
+            }
+
+            if (comp == 3 && you < comp)
+            {
+                MessageBox.Show("The winner is Computer. Congratulations!", "Result", MessageBoxButtons.OK);
+                rbBato.Checked = false;
+                rbGunting.Checked = false;
+                rbPapel.Checked = false;
+                comp = 0;
+                you = 0;
+                lblYou.Text = you.ToString();
+                lblComputer.Text = comp.ToString();
+            }
+            else if (you == 3 && you > comp)
+            {
+                MessageBox.Show("The winner is User. Congratulations!", "Result", MessageBoxButtons.OK);
+                rbBato.Checked = false;
+                rbGunting.Checked = false;
+                rbPapel.Checked = false;
+                comp = 0;
+                you = 0;
+                lblYou.Text = you.ToString();
+                lblComputer.Text = comp.ToString();
+            }
+
+        }
+    }
+}
